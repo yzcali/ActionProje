0c6816c365c1d561badf982e9de10ea5d22e48da
yzcali <yaziciali694@gmail.com>
2020-12-10 13:26:48 +0100

    getCommaands

---
+++ src/test/java/F_smokeTest/GetCommands.java
@@ -0,0 +1,26 @@
+package F_smokeTest;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import utilities.Testbase;
+
+public class GetCommands extends Testbase {
+    @Test
+    public void getC(){
+        driver.get("https://nike.com"); // open the url
+
+        System.out.println(driver.getTitle()); // returns the title of the page
+        //Nike. Just Do It. Nike FR
+        System.out.println(driver.getCurrentUrl());  // returns the url of the page
+         //https://www.nike.com/fr/
+       String text= driver.findElement(By.id("hf_title_signin_membership")).getText();
+        System.out.println(text);
+
+       driver.quit ();
+
+
+
+
+
+    }
+}
